ca82239d00d3911ae06583542b204eea8a26a763
1F10220206 <s1f102202066@iniad.org>
2023-07-06 13:41:16 +0900

    ソート機能を追加

--- todo/tests.py
+++ todo/tests.py
@@ -3,7 +3,11 @@ from django.utils import timezone
 from datetime import datetime
 from todo.models import Task
 
-class TaskModelTest(TestCase):
+class SampleTestCase(TestCase):
+    def test_sample1(self):
+        self.assertEqual(1 + 2, 3)
+
+class TaskModelTestCase(TestCase):
     def test_create_task1(self):
         due = timezone.make_aware(datetime(2023,6,30,23,59,59))
         task = Task(title='task1', due_at=due)
@@ -57,9 +61,18 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(len(response.context['tasks']),0)
 
     def test_index_post(self):
+        client = Client()
+        data = {'title':'Test Task', 'due_at':'2023-06-30 23:59:59'}
+        response = client.post('/',data)
+
+        self.assertEqual(response.status_code,200)
+        self.assertEqual(response.templates[0].name, 'todo/index.html')
+        self.assertEqual(len(response.context['tasks']),1)
+
+    def test_index_get_order_post(self):
         task1 = Task(title='task1',due_at = timezone.make_aware(datetime(2023, 7, 1)))
         task1.save()
-        task2 = Task(titlle='task2',due_at = timezone.make_aware(datetime(2023, 8, 1)))
+        task2 = Task(title='task2',due_at = timezone.make_aware(datetime(2023, 8, 1)))
         task2.save()
         client = Client()
         response = client.get('/?order=post')
@@ -72,12 +85,12 @@ class TodoViewTestCase(TestCase):
     def test_index_get_order_due(self):
         task1 = Task(title='task1',due_at = timezone.make_aware(datetime(2023, 7, 1)))
         task1.save()
-        task2 = Task(titlle='task2',due_at = timezone.make_aware(datetime(2023, 8, 1)))
+        task2 = Task(title='task2',due_at = timezone.make_aware(datetime(2023, 8, 1)))
         task2.save()
         client = Client()
         response = client.get('/?order=due')
 
         self.assertEqual(response.status_code,200)
         self.assertEqual(response.templates[0].name,'todo/index.html')
-        self.assertEqual(response.context['tasks'][0],task2)
-        self.assertEqual(response.context['tasks'][1],task1)
+        self.assertEqual(response.context['tasks'][0],task1)
+        self.assertEqual(response.context['tasks'][1],task2)
